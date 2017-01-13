1b1bcbf0ae13ac9a36e4cb81c651f9304d080fb2
totvs-djorge <diorge.jorge@totvs.com.br>
2017-01-13 14:38:35 -0300

    vers 0.1

--- models/projeto.js
+++ models/projeto.js
@@ -7,7 +7,20 @@ function Projeto() {
        con.release();
        res.send(result);
      });
-   });
- };
-}
+   })};
+ this.listTarefasProjeto = function(res) {
+  connection.acquire(function(err, con) {
+    con.query('select projects.name \'projectName\',issues.description,users.id \'userID\',issues.done_ratio,issues.id,issues.due_date,issues.subject,issues.estimated_hours,users.firstname,users.lastname,issue_statuses.name from issues inner join users on users.id = issues.assigned_to_id inner join issue_statuses on issue_statuses.id = issues.status_id inner join projects on projects.id = issues.project_id where due_date < now()', function(err, result) {
+      con.release();
+      res.send(result);
+    });
+  })};
+  this.listTarefasProjetoCliente = function(id,res) {
+   connection.acquire(function(err, con) {
+     con.query('select projects.name \'projectName\',issues.description,users.id \'userID\',issues.done_ratio,issues.id,issues.due_date,issues.subject,issues.estimated_hours,users.firstname,users.lastname,issue_statuses.name from issues inner join users on users.id = issues.assigned_to_id inner join issue_statuses on issue_statuses.id = issues.status_id inner join projects on projects.id = issues.project_id where due_date < now() and users.id = ?',[id], function(err, result) {
+       con.release();
+       res.send(result);
+     });
+   })};
+};
 module.exports = new Projeto();
--- public/redmine.js
+++ public/redmine.js
@@ -1,4 +1,4 @@
-var redmineApp = angular.module('redmineApp',[]);
+var redmineApp = angular.module('redmineApp',['angular.filter']);
 //inicio controller
 redmineApp.filter('getAge', function() {
     return function(input) {
@@ -14,16 +14,45 @@ redmineApp.filter('renderHTML', function($sce) {
     };
   }
 );
-function mainController($scope,$http){
+redmineApp.controller('mainController',function ($scope,$http){
   $scope.formData = {};
   $http.get('/tarefas/cliente/p')
-        .success(function( data ) {
-          $scope.tarefas = data;
-          console.log(data);
-        })
-        .error(function(data){
-          console.log('Error: '+ data);
-        });
+  .then(function successCallback(response) {
+    $scope.tarefas = response.data;
+    console.log(response.data);
+  }, function errorCallback(response) {
+    console.log('Error: '+ response.data);
+  });
+});
+redmineApp.controller('tarefasProjetoController',function ($scope,$http){
+  $scope.formData = {};
+  $http.get('/projeto/tarefas/')
+  .then(function successCallback(response) {
+    $scope.tarefas = response.data;
+    console.log(response.data);
+  }, function errorCallback(response) {
+    console.log('Error: '+ response.data);
+  });
+});
 
+redmineApp.controller('tarefasProjetoBanparaController',function ($scope,$http){
+  $scope.formData = {};
+  $http.get('/projeto/tarefasCliente/28/')
+  .then(function successCallback(response) {
+    $scope.tarefas = response.data;
+    console.log(response.data);
+  }, function errorCallback(response) {
+    console.log('Error: '+ response.data);
+  });
+});
 
-}
+redmineApp.controller('tarefasProjetoBasaController',function ($scope,$http){
+  $scope.formData = {};
+  $http.get('/projeto/tarefasCliente/27/')
+  .then(function successCallback(response) {
+    $scope.tarefas = response.data;
+    console.log(response.data);
+  }, function errorCallback(response) {
+    console.log('Error: '+ response.data);
+  });
+});
